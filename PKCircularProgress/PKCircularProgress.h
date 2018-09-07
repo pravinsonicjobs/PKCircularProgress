20863bb98cfcbba30e0958b83bcdb7883175c3dc
Pravin Kulkarni <pravinkulkarni@pravin-kulkarni-mac.local>
2018-09-07 13:05:04 +0530

    Initial Commit

---
+++ PKCircularProgress/PKCircularProgress.h
@@ -0,0 +1,19 @@
+//
+//  PKCircularProgress.h
+//  PKCircularProgress
+//
+//  Created by Pravin Kulkarni on 07/09/18.
+//  Copyright © 2018 Pravin Kulkarni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PKCircularProgress.
+FOUNDATION_EXPORT double PKCircularProgressVersionNumber;
+
+//! Project version string for PKCircularProgress.
+FOUNDATION_EXPORT const unsigned char PKCircularProgressVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PKCircularProgress/PublicHeader.h>
+
+
